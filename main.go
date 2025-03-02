403e9fb32aef924fe6b32377dc8e43431f10e6bb
Ziahh <2787495751@qq.com>
2025-03-02 11:20:20 +0800

    init

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/Spieny/AquaMall/common"
+	"github.com/Spieny/AquaMall/controllers"
+	_ "github.com/Spieny/AquaMall/routers"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/astaxie/beego/session/redis"
+)
+
+func main() {
+
+}
